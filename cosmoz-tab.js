f7d842ce5ad2b29b601e3f6ef3db786af4eb31a2
Meghea Iulian <iulian.meghea@gmail.com>
2017-08-24 20:38:48 +0300

    [jsdoc] Partial jsdoc for cosmoz-tab

--- cosmoz-tab.js
+++ cosmoz-tab.js
@@ -1,19 +1,33 @@
 // @license Copyright (C) 2015 Neovici AB - Apache 2 License
-/*global Cosmoz, CosmozTabs,Polymer, document, window */
+/*global CosmozTabs */
 
 (function () {
 	'use strict';
-
 	Polymer({
+		/**
+		 * Fired when `disabled`, `hidden`, `heading`, `icon`, `selectedIcon`, `iconColor`, `badge`
+		 * properties are changed.
+		 *
+		 * @event cosmoz-tab-property-changed
+		 * @param  {Object} event.detail The event detail object
+		 * @param  {HTMLElement} event.detail.tab The current cosmoz-tab instance
+		 * @param  {String} event.detail.propertyName The name of changed the property
+		 */
 		is: 'cosmoz-tab',
 		properties: {
 
+			/**
+			 * True if the tab is opened.
+			 */
 			opened: {
 				type: Boolean,
 				readOnly: true,
 				notify: true
 			},
 
+			/**
+			 * If true, the tab will be hidden
+			 */
 			hidden: {
 				type: Boolean,
 				value: false,
@@ -21,6 +35,9 @@
 				observer: '_hiddenChanged'
 			},
 
+			/**
+			 *  If true, the tab will be disabled
+			 */
 			disabled: {
 				type: Boolean,
 				value: false,
@@ -28,47 +45,75 @@
 				observer: '_disabledChanged'
 			},
 
+			/**
+			 * The tab's heading text.
+			 */
 			heading: {
 				type: String,
 				observer: '_headingChanged'
 			},
 
+			/**
+			 * Indicates wether the parent `cosmoz-tabs` should be displayed using an accordion.
+			 * Bound to same  property on parent `cosmoz-tabs`.
+			 */
 			accordion: {
 				type: Boolean,
 				readOnly: true
 			},
 
+			/**
+			 * True if the parent `cosmoz-tabs` has class `fit` or `flex`.
+			 * Bound to same  property on parent `cosmoz-tabs`.
+			 */
 			flex: {
 				type: Boolean,
 				readOnly: true
 			},
-
+			/**
+			 * The icon of the tab.
+			 */
 			icon: {
 				type: String,
 				value: 'radio-button-unchecked',
 				observer: '_iconChanged'
 			},
 
+			/**
+			 * The icon of the tab when it is selected.
+			 */
 			selectedIcon: {
 				type: String,
 				value: 'radio-button-checked',
 				observer: '_selectedIconchanged'
 			},
 
+			/**
+			 *  The color of the tab's icon.
+			 */
 			iconColor: {
 				type: String,
 				value: '#15b0d3',
 				observer: '_iconColorChanged'
 			},
 
+			/**
+			 *  The id of tab.
+			 */
 			tabId: {
 				type: String
 			},
 
+			/**
+			 *  The class used for the tab's content element.
+			 */
 			_tabContentClass: {
 				type: String
 			},
 
+			/**
+			 * True if tab contains `cosmoz-tab-card` elements.
+			 */
 			_useCards: {
 				type: Boolean
 			},
@@ -81,17 +126,26 @@
 				type: Boolean
 			},
 
+			/**
+			 * The array of `cosmoz-tab-card` that this element contains.
+			 */
 			cards: {
 				type: Array,
 				readOnly: true,
 				value: false
 			},
 
+			/**
+			 * When set the element will contain a `paper-bagde` with the label set to this property.
+			 */
 			badge: {
 				type: String,
 				observer: '_badgeChanged'
 			},
 
+			/**
+			 * True when `badge` propery is set.
+			 */
 			_showBadge: {
 				type: Boolean,
 				value: false,
@@ -108,18 +162,34 @@
 			CosmozTabs.BindParentChildBehavior
 		],
 
+		/**
+		 * Element attached lifecycle callback that binds parent properties
+		 * and creates `_cardsObserver` DOM mutation observer that calls `_updateCards` on change.
+		 * @return {void}
+		 */
 		attached: function () {
 			this.bindParentProperty('accordion');
 			this.bindParentProperty('flex');
 			this._cardsObserver = Polymer.dom(this.$.tabContent).observeNodes(this._updateCards.bind(this));
 		},
 
+		/**
+		 * Element detached lifecycle callback
+		 * that removes `_cardsObserver` DOM mutation observer.
+		 * @return {void}
+		 */
 		detached: function () {
 			if (this._cardsObserver) {
 				Polymer.dom(this.$.tabContent).unobserveNodes(this._cardsObserver);
 			}
 		},
 
+		/**
+		 * Fires `cosmoz-tab-property-change` event
+		 * @param  {String} propertyName The property that changed
+		 * @fires cosmoz-tab-property-change
+		 * @return {void}
+		 */
 		_fireTabPropertyChanged: function (propertyName) {
 			this.fire('cosmoz-tab-property-changed', { tab: this, propertyName: propertyName});
 		},
@@ -152,6 +222,12 @@
 			this._fireTabPropertyChanged('badge');
 		},
 
+		/**
+		 * Computes `_showBadge` property if `badge` propery is set to non-empty value.
+		 *
+		 * @param  {String} badge The badge propery
+		 * @return {Boolean} True if badge should be shown
+		 */
 		_computeShowBadge: function (badge) {
 			return badge !== undefined && badge !== null && badge !== 0 && badge !== '';
 		},
