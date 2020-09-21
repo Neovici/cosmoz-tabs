b21120d35a371347a39779a6c225fa4aa271389a
Meghea Iulian <iulian.meghea@gmail.com>
2020-09-21 20:21:00 +0300

    chore(deps): update and cleanup

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@
 with import
   (
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/6152513.tar.gz";
-      sha256 = "1gzwz9jvfcf0is6zma7qlgszkngfb2aa4kam0nhs0qnwb4nqn7mg";
+      url = "https://github.com/NixOS/nixpkgs/archive/2a35f66.tar.gz";
+      sha256 = "1ac01hyvniiwrwgqlvmx76dxc7aqg71nx3d05d0dc35lbyjq7acf";
     }
   )
 { };
