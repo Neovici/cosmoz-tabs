5c385377d8917fdbe77c714745cc798d7ad659d9
Meghea Iulian <iulian.meghea@gmail.com>
2020-09-22 21:25:37 +0300

    fix(cosmoz-tabs): adds back tab-select and tab-first-select events

--- test/cosmoz-tabs-events.test.js
+++ test/cosmoz-tabs-events.test.js
@@ -1,22 +1,24 @@
 import {
 	assert,
 	html,
-	fixtureSync
+	fixtureSync,
+	fixture,
+	nextFrame,
+	oneEvent
 } from '@open-wc/testing';
 import {
 	assert as sinonAssert,
 	spy
 } from 'sinon';
 
-import { flush } from '@polymer/polymer/lib/utils/flush';
 import '../cosmoz-tabs.js';
 
-suite('events', () => {
+suite('cosmoz-tabs', () => {
 	let tabs;
 
 	setup(async () => {
 		tabs = await fixtureSync(html`
-			<cosmoz-tabs fallback-selection=''>
+			<cosmoz-tabs>
 				<cosmoz-tab name="t1" heading="Tab0">1</cosmoz-tab>
 				<cosmoz-tab name="t2" heading="Tab1">2</cosmoz-tab>
 				<cosmoz-tab name="t3" heading="Tab2">3</cosmoz-tab>
@@ -25,32 +27,36 @@ suite('events', () => {
 	});
 
 	test('fires tab-select event', async () => {
-		assert.isUndefined(tabs.selected);
-		const onSelect = spy();
-		tabs.items[2].addEventListener('tab-select', onSelect);
-		tabs.selected = 't3';
-		tabs.items[2].removeEventListener('tab-select', onSelect);
-		sinonAssert.calledOnce(onSelect);
+		setTimeout(() => {
+			tabs.selected = 't3';
+		});
+		await oneEvent(tabs.querySelector('[name=t3]'), 'tab-select');
 	});
 
-	test('fires tab-first-select event only once before tab-select', () => {
+	test('fires tab-first-select event only once before tab-select', async () => {
 		const onFirstSelect = spy(),
-			onSelect = spy();
+			onSelect = spy(),
+			tab = tabs.querySelector('[name=t3]');
 
 		assert.isUndefined(tabs.selected);
 
-		tabs.items[2].addEventListener('tab-first-select', onFirstSelect);
-		tabs.items[2].addEventListener('tab-select', onSelect);
+		tab.addEventListener('tab-first-select', onFirstSelect);
+		tab.addEventListener('tab-select', onSelect);
 
-		tabs.selected = 't3';
-		flush();
+		setTimeout(() => {
+			tabs.selected = 't3';
+		});
+		await oneEvent(tab, 'tab-select');
 		sinonAssert.calledOnce(onFirstSelect);
 		sinonAssert.calledOnce(onSelect);
 		sinonAssert.callOrder(onFirstSelect, onSelect);
 
 		onSelect.resetHistory();
 		onFirstSelect.resetHistory();
-		tabs.selected = 't2';
+		setTimeout(() => {
+			tabs.selected = 't2';
+		});
+		await oneEvent(tabs.querySelector('[name=t2]'), 'tab-select');
 		sinonAssert.notCalled(onFirstSelect);
 		sinonAssert.notCalled(onSelect);
 
@@ -58,7 +64,14 @@ suite('events', () => {
 		onFirstSelect.resetHistory();
 
 		tabs.selected = 't3';
+		setTimeout(() => {
+			tabs.selected = 't3';
+		});
+		await oneEvent(tab, 'tab-select');
 		sinonAssert.notCalled(onFirstSelect);
 		sinonAssert.calledOnce(onSelect);
+
+		tab.removeEventListener('tab-first-select', onFirstSelect);
+		tab.removeEventListener('tab-select', onSelect);
 	});
 });
