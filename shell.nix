c0ba8a03f2c9de267b4d99df09eab9b4560748b5
Meghea Iulian <iulian.meghea@gmail.com>
2020-10-06 10:27:42 +0300

    chore(deps): update minor dev deps

--- shell.nix
+++ shell.nix
@@ -3,15 +3,22 @@
 with import
   (
     builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/2a35f66.tar.gz";
-      sha256 = "1ac01hyvniiwrwgqlvmx76dxc7aqg71nx3d05d0dc35lbyjq7acf";
+      url = "https://github.com/NixOS/nixpkgs/archive/daaa0e33505082716beb52efefe3064f0332b521.tar.gz";
+      sha256 = "15vprzpbllp9hy5md36ch1llzhxhd44d291kawcslgrzibw51f95";
     }
   )
 { };
 mkShell {
-  buildInputs = [ nodejs-12_x python3 firefox jre nodePackages.prettier ];
+  buildInputs = [ nodejs-12_x python3 firefox jre azure-storage-azcopy google-chrome ];
   shellHook = ''
     export CHROME_BIN=${google-chrome}/bin/google-chrome-stable
     export PATH=$(npm bin):$PATH
+
+    link_selenium () {
+      if [ -d "./node_modules" ]; then
+        ln -sfnv "${chromedriver}/bin/chromedriver" "$(find "./node_modules" -path '**/chromedriver/*-chromedriver')"
+        ln -sfnv "${geckodriver}/bin/geckodriver" "$(find "./node_modules" -path '**/geckodriver/*-geckodriver')"
+      fi
+    }
   '';
 }
