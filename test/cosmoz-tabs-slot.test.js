af6f12d8c38f7141fd88f86e32c8dadc48ebcf3f
Meghea Iulian <iulian.meghea@gmail.com>
2020-10-13 17:29:37 +0300

    fix: collect cosmoz-tab element from nested slots
    This collects nested slotted cosmoz-tab elements:
    slot> slot> cosmoz-tab

---
+++ test/cosmoz-tabs-slot.test.js
@@ -0,0 +1,34 @@
+import {
+	assert, html, fixture, nextFrame
+} from '@open-wc/testing';
+
+import '../cosmoz-tabs.js';
+import { component } from 'haunted';
+
+suiteSetup(() => {
+	if (customElements.get('slot-test')) {
+		return;
+	}
+	customElements.define('slot-test', component(() => html`
+	<cosmoz-tabs>
+		<cosmoz-tab name="tab0" heading="Tab0">1</cosmoz-tab>
+		<slot></slot>
+	</cosmoz-tabs>
+`));
+});
+
+suite('cosmoz-tabs slot', () => {
+	test('collects cosmoz-tabs from slot', async () => {
+		const el = await fixture(html`
+			<slot-test>
+				<cosmoz-tab name="tab1">2</cosmoz-tab>
+				<span name="notatab2">3</span>
+			</slot-test>
+		`),
+			tabs = el.shadowRoot.querySelector('cosmoz-tabs');
+		await nextFrame();
+		tabs.selected = 'tab1';
+		await nextFrame();
+		assert.equal(el.querySelector('cosmoz-tab').getAttribute('is-selected'), '');
+	});
+});
