16121bebe64803d6e4e9f736d0d079151101de32
megheaiulian <iulian@plumelo.com>
2024-06-04 08:45:41 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-tabs@ffed59431fc75e1eb16a7794d071627d5277bb57 🚀

---
+++ entry-preview-CT1YEuLY.js
@@ -0,0 +1,8 @@
+export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-DhB0gFDo.js';
+import './index-Kjm4kNkQ.js';
+import './lit-html-diQ3t45r.js';
+import './directive-helpers-CRujXBpd.js';
+
+var parameters={renderer:"web-components"};
+
+export { parameters };
