735f60c089bf1e756e16e44680d8332c0b3b166d
megheaiulian <megheaiulian@users.noreply.github.com>
2024-08-20 13:47:58 +0000

    Remove preview for PR 218 🛬

--- pr/pr-218/entry-preview-CT1YEuLY.js
+++
@@ -1,8 +0,0 @@
-export { r as render, a as renderToCanvas } from './chunk-FJPRWHXQ-DhB0gFDo.js';
-import './index-Kjm4kNkQ.js';
-import './lit-html-diQ3t45r.js';
-import './directive-helpers-CRujXBpd.js';
-
-var parameters={renderer:"web-components"};
-
-export { parameters };
