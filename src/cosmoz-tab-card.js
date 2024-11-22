ff694177ad547d4e55634ea9ebce3f9eb94713cc
Asrinne Nazari <asrinne.nazari@neovici.se>
2024-11-22 11:59:32 +0100

    feat(cosmoz-tab-card): add css rules for heading BREAKING CHANGE: `cosmoz-tab-card` heading must be an attribute

--- src/cosmoz-tab-card.js
+++ src/cosmoz-tab-card.js
@@ -85,6 +85,14 @@ const style = css`
 		width: var(--cosmoz-tab-card-width, 300px);
 	}
 
+	:host([heading]) h1 {
+		display: block;
+	}
+
+	h1.heading {
+		display: none;
+	}
+
 	.collapse {
 		display: flex;
 		flex-direction: column;
