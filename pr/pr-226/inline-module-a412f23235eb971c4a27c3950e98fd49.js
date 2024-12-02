978ff42beba234581c9daa413cce16f8749c6c9e
megheaiulian <megheaiulian@users.noreply.github.com>
2024-12-02 15:22:17 +0000

    Deploy preview for PR 226 🛫

---
+++ pr/pr-226/inline-module-a412f23235eb971c4a27c3950e98fd49.js
@@ -0,0 +1,40 @@
+import './sb-preview/runtime.js';
+
+const channel = __STORYBOOK_MODULE_CHANNEL_POSTMESSAGE__.createChannel({ page: "preview" });
+__STORYBOOK_MODULE_PREVIEW_API__.addons.setChannel(channel);
+window.__STORYBOOK_ADDONS_CHANNEL__ = channel;
+const { SERVER_CHANNEL_URL } = globalThis;
+if (SERVER_CHANNEL_URL) {
+  const serverChannel = __STORYBOOK_MODULE_CHANNEL_WEBSOCKET__.createChannel({ url: SERVER_CHANNEL_URL });
+  __STORYBOOK_MODULE_PREVIEW_API__.addons.setServerChannel(serverChannel);
+  window.__STORYBOOK_SERVER_CHANNEL__ = serverChannel;
+}
+
+const importers = {
+  "./stories/cosmoz-tab-card.stories.js": () => import('./cosmoz-tab-card.stories-BvCXuxNf.js'),
+  "./stories/cosmoz-tab.stories.js": () => import('./cosmoz-tab.stories-C_G3FYr7.js'),
+  "./stories/cosmoz-tabs-next.stories.js": () => import('./cosmoz-tabs-next.stories-JTEO-IIn.js'),
+  "./stories/cosmoz-tabs.stories.js": () => import('./cosmoz-tabs.stories-BGUHBlxh.js')
+};
+function importFn(path) {
+  return importers[path]();
+}
+
+const getProjectAnnotations = async () => {
+  const configs = await Promise.all([
+    import('./entry-preview-BRopiGfa.js'),
+    import('./entry-preview-docs-BpGAFbCw.js'),
+    import('./preview-C8OmhiPB.js'),
+    import('./preview-BcJVSA4a.js'),
+    import('./preview-Bf0d1tVO.js'),
+    import('./preview-CU2JNqMw.js'),
+    import('./preview-BT9ta7nr.js'),
+    import('./preview-CDagOaLU.js'),
+    import('./preview-KqRhvb1R.js')
+  ]);
+  return __STORYBOOK_MODULE_PREVIEW_API__.composeConfigs(configs);
+};
+window.__STORYBOOK_PREVIEW__ = window.__STORYBOOK_PREVIEW__ || new __STORYBOOK_MODULE_PREVIEW_API__.PreviewWeb();
+window.__STORYBOOK_STORY_STORE__ = window.__STORYBOOK_STORY_STORE__ || window.__STORYBOOK_PREVIEW__.storyStore;
+window.__STORYBOOK_CLIENT_API__ = window.__STORYBOOK_CLIENT_API__ || new __STORYBOOK_MODULE_PREVIEW_API__.ClientApi({ storyStore: window.__STORYBOOK_PREVIEW__.storyStore });
+window.__STORYBOOK_PREVIEW__.initialize({ importFn, getProjectAnnotations });
