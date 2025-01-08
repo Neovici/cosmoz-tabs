83793637c5ed5cefa01f0b6caa36c3e60ace0e85
AsrinneN <AsrinneN@users.noreply.github.com>
2025-01-08 10:45:44 +0000

    Deploy preview for PR 227 🛫

---
+++ pr/pr-227/cosmoz-tab.stories-HWkqm9TD.js
@@ -0,0 +1,76 @@
+import { x } from './lit-html-CmtJAihu.js';
+import './cosmoz-tabs-BgsztwmZ.js';
+import './if-defined-Bb7yiZFq.js';
+
+var cosmozTab_stories = {
+  title: "Tab",
+  component: "cosmoz-tab"
+};
+const basics = () => x`
+	<cosmoz-tabs selected="tab3">
+		<cosmoz-tab heading="Tab1" name="tab1" disabled>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+				massa ante, maximus in consectetur non, imperdiet ullamcorper risus.
+				Donec vulputate justo nibh.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab
+			heading="Tab2"
+			name="tab2"
+			badge="2"
+			icon-color="red"
+			icon="icons:check-box-outline-blank"
+			selected-icon="icons:check-box"
+		>
+			<p>
+				Fusce consectetur nisi at felis finibus rutrum. Vestibulum fermentum
+				pharetra sem, vitae tincidunt est mattis tristique. Donec blandit nulla
+				non tellus tincidunt pretium. Phasellus et purus id dolor venenatis
+				mollis.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab heading="Tab3" name="tab3" badge="3" hidden>
+			<p>
+				Etiam ante dolor, commodo non vestibulum vel, malesuada a nunc.
+				Vestibulum accumsan, sapien eu gravida consectetur, purus felis lobortis
+				massa, id consequat eros lacus sit amet quam. Nunc bibendum elit turpis.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab heading="Tab4" name="tab4" badge="4">
+			<p>
+				Sed pretium a felis sed dictum. Vestibulum ornare quam in metus vehicula
+				consectetur. Proin faucibus ac sapien vel bibendum. Quisque molestie
+				cursus lacus. Maecenas sit amet finibus diam, eget finibus nisi. Integer
+				eget laoreet metus. Fusce quis molestie massa.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab heading="Tab5" name="tab5" badge="4">
+			<p>
+				Sed pretium a felis sed dictum. Vestibulum ornare quam in metus vehicula
+				consectetur. Proin faucibus ac sapien vel bibendum. Quisque molestie
+				cursus lacus. Maecenas sit amet finibus diam, eget finibus nisi. Integer
+				eget laoreet metus. Fusce quis molestie massa.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab heading="Tab6" name="tab6">
+			<p>
+				Sed pretium a felis sed dictum. Vestibulum ornare quam in metus vehicula
+				consectetur. Proin faucibus ac sapien vel bibendum. Quisque molestie
+				cursus lacus. Maecenas sit amet finibus diam, eget finibus nisi. Integer
+				eget laoreet metus. Fusce quis molestie massa.
+			</p>
+		</cosmoz-tab>
+		<cosmoz-tab heading="Tab7" name="tab7">
+			<p>
+				Sed pretium a felis sed dictum. Vestibulum ornare quam in metus vehicula
+				consectetur. Proin faucibus ac sapien vel bibendum. Quisque molestie
+				cursus lacus. Maecenas sit amet finibus diam, eget finibus nisi. Integer
+				eget laoreet metus. Fusce quis molestie massa.
+			</p>
+		</cosmoz-tab>
+	</cosmoz-tabs>
+`;
+const __namedExportsOrder = ["basics"];
+
+export { __namedExportsOrder, basics, cosmozTab_stories as default };
